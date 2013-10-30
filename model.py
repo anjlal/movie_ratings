98fc4c9740b8a3b4ace7d6873a0711a4e516c54d
Angie Lal <angielal@gmail.com>
2013-10-29 18:18:55 -0700

    Now mulithreaded and has email and password fields for User.

--- model.py
+++ model.py
@@ -1,12 +1,16 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, Date
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from datetime import datetime
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
 
-ENGINE = None
-Session = None
+engine = create_engine("sqlite:///ratings2.db", echo=False)
+session = scoped_session(sessionmaker(bind=engine, autocommit = False, autoflush = False))
+
 Base = declarative_base()
+Base.query = session.query_property()
 
 ### Class declarations go here
 class User(Base):
@@ -15,18 +19,19 @@ class User(Base):
     id = Column(Integer, primary_key = True)
     age = Column(Integer)
     gender = Column(String(1))
+    occupation = Column(String(100))
     zipcode = Column(String(15))
-    email = Column(String(64), nullable=True)
-    password = Column(String(64), nullable=True)
+    email = Column(String(64), nullable = True)
+    password = Column(String(64), nullable = True)
 
     
-    def __init__(self, user_id, age, gender, zipcode, email=None, password=None):
-        self.id = user_id
-        self.age = age
-        self.gender = gender
-        self.zipcode = zipcode
-        self.email = email
-        self.password = password
+    # def __init__(self, user_id, age, gender, zipcode, email=None, password=None):
+    #     self.id = user_id
+    #     self.age = age
+    #     self.gender = gender
+    #     self.zipcode = zipcode
+    #     self.email = email
+    #     self.password = password
     
 class Movie(Base):
     __tablename__ = "movies"
@@ -36,38 +41,42 @@ class Movie(Base):
     released_at = Column(Date())
     imdb_url = Column(String(128))
     
-    def __init__(self, movie_id, name, released_at, imdb_url):
-        self.id = movie_id
-        self.name = name
-        self.released_at = released_at
-        self.imdb_url = imdb_url
+    # def __init__(self, movie_id, name, released_at, imdb_url):
+    #     self.id = movie_id
+    #     self.name = name
+    #     self.released_at = released_at
+    #     self.imdb_url = imdb_url
 
 class Rating(Base):
     __tablename__ = "ratings"
     
-    rating_id = Column(Integer, primary_key=True)
-    user_id = Column(Integer)
-    movie_id = Column(Integer)
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    movie_id = Column(Integer, ForeignKey('movies.id'))
     rating = Column(Integer)
     timestamp = Column(Integer)
     
-    def __init__(self, rating_id, user_id, movie_id, rating, timestamp):
-        self.id = rating_id
-        self.user_id = user_id
-        self.movie_id = movie_id
-        self.rating = rating
-        self.timestamp = timestamp
+    user = relationship("User", backref=backref("ratings", order_by=id))
+    movie = relationship("Movie", backref=backref("ratings", order_by=id))
     
+    # def __init__(self, rating_id, user_id, movie_id, rating, timestamp):
+    #     self.id = rating_id
+    #     self.user_id = user_id
+    #     self.movie_id = movie_id
+    #     self.rating = rating
+    #     self.timestamp = timestamp
+    # 
 
 ### End class declarations
-def connect():
-    global ENGINE
-    global Session
-    
-    ENGINE = create_engine("sqlite:///ratings.db", echo=True)
-    Session = sessionmaker(bind=ENGINE)
-    
-    return Session()
+# Not needed because using scoped_session for multithreadedness
+# def connect():
+#     # global ENGINE
+#     # global Session
+#     # 
+#     # ENGINE = create_engine("sqlite:///ratings2.db", echo=True)
+#     # Session = sessionmaker(bind=ENGINE)
+#     
+#     # return Session()
 def main():
     """In case we need this for something"""
     pass
--- seed.py
+++ seed.py
@@ -10,22 +10,27 @@ def load_users(session):
     while (line):
         data = line.strip("\n").split("|")
         line = _f.readline()
-        
-        user_id = int(data[0])
-        age = int(data[1])
-        gender = data[2]
-        zipcode = data[3]
+        user = model.User()
+        user.id = int(data[0])
+        user.age = int(data[1])
+        user.gender = data[2]
+        user.occupation = data[3]
+        user.zipcode = data[4]
+        # user_id = int(data[0])
+        # age = int(data[1])
+        # gender = data[2]
+        # zipcode = data[3]
+        #     
+        # if len(data) == 4:
+        #     email = None
+        # #     password = None
+        # if len(data) == 5:
+        #     user.email = data[4]
+        #     # password = None
+        # if len(data) == 6:
+        #     user.password = data[5]
     
-        if len(data) == 4:
-            email = None
-            password = None
-        if len(data) == 5:
-            email = data[4]
-            password = None
-        if len(data) == 6:
-            password = data[5]
-    
-        user = model.User(user_id, age, gender, zipcode, email, password)
+        # user = model.User(user_id, age, gender, zipcode, email, password)
         session.add(user)
         session.commit()
 
@@ -36,13 +41,14 @@ def load_movies(session):
         reader = csv.reader(f, delimiter="|")
         try:
             for row in reader:
-                movie_id = int(row[0])
+                movie = model.Movie()
+                movie.id = int(row[0])
                 title = re.sub(' \(\d\d\d\d\)$', '', row[1])
-                title = title.decode("latin-1")
+                movie.name = title.decode("latin-1")
                 if row[2] != "":
-                    release_date = datetime.strptime(row[2], "%d-%b-%Y").date()
-                imdb_url = row[4]
-                movie = model.Movie(movie_id, title, release_date, imdb_url)
+                    movie.released_at = datetime.strptime(row[2], "%d-%b-%Y").date()
+                movie.imdb_url = row[4]
+                # movie = model.Movie(movie_id, title, release_date, imdb_url)
                 session.add(movie)
                 session.commit()
         except csv.Error as e:
@@ -56,20 +62,30 @@ def load_ratings(session):
         reader = csv.reader(f, delimiter="\t")
         try:
             for row in reader:
-                rating_id = count
-                user_id = int(row[0])
-                movie_id = int(row[1])
-                rating = int(row[2])
-                timestamp = int(row[3])
+                rating = model.Rating()
+                rating.id = count
+                rating.user_id = int(row[0])
+                rating.movie_id = int(row[1])
+                rating.rating = int(row[2])
+                rating.timestamp = int(row[3])
                 
                 count += 1
                 
-                rating = model.Rating(rating_id, user_id, movie_id, rating, timestamp)
+                # rating = model.Rating(rating_id, user_id, movie_id, rating, timestamp)
                 session.add(rating)
-                session.commit()                
+                session.commit()
         except csv.Error as e:
             sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
     
+
+# def main(session):
+#     # You'll call each of the load_* functions with the session as an argument
+#     load_users(session)
+#     load_movies(session)
+#     load_ratings(session)
+# if __name__ == "__main__":
+#     #s= model.connect()
+#     main(s)
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
@@ -77,5 +93,5 @@ def main(session):
     load_movies(session)
     load_ratings(session)
 if __name__ == "__main__":
-    s= model.connect()
-    main(s)
+    #s= model.connect()
+    main(model.session)
